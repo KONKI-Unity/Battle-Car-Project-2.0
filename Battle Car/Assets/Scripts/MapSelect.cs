80cf9c548c29826ad5527b848f09adfbf562e0ff
Pyrd <Mathieu.cailly@gmail.com>
2018-06-06 00:16:02 +0200

    -adding selectMap

--- Battle Car/Assets/Scripts/MapSelect.cs
+++ Battle Car/Assets/Scripts/MapSelect.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class MapSelect : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private NetworkManager networkManager;
+
+    [SerializeField]
+    private const int mapCount = 3;
+
+    [SerializeField]
+    private string[] sceneName = new string[mapCount] { "dev_map", "map2", "map3"};
+
+    
+
+    private int currentScene;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        currentScene = 0;
+        ChangeMap(0);
+    }
+
+    
+
+    public void ChangeMap(int value)
+    {
+        if (value >= mapCount || value < 0)
+        {
+            Debug.Log("Wrong index");
+        }
+        else
+        {
+            currentScene = value;
+            networkManager.onlineScene = ("Scenes/" + sceneName[currentScene]);
+            Debug.Log("Server has changed map to : " + sceneName[currentScene]);
+        }
+            
+        
+    }
 }
