107c01ac1f01c6e69066df731a4d075744652ca4
Pyrd <Mathieu.cailly@gmail.com>
2018-06-06 02:04:26 +0200

    -enhance mainMenu

---
+++ Battle Car/Assets/Scripts/MainMenuSelector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSelector : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] buttonSet;
+
+    private int currentTab = 0;
+
+    private void Start()
+    {
+        currentTab = 0;
+        SetTab(0);
+    }
+
+    private void Reset()
+    {
+        for(int i = 0; i < buttonSet.Length; i++)
+        {
+            buttonSet[i].active = false;
+        }
+    }
+
+    public void SetTab(int index)
+    {
+        currentTab = index;
+        Reset();
+        buttonSet[index].active = true;
+        Debug.Log(buttonSet[index].name + " has been loaded");
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
