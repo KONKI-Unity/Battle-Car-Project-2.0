1f10c561d14d8358d6cb4e60b50c0d195588282c
rayan <rayan.sekkat@epita.fr>
2018-06-06 17:00:37 +0200

    -rayan

---
+++ Battle Car/Assets/Scripts/Spawn weapon/PickUp.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+    public GameObject inventoryPanel;
+    public GameObject[] inventoryIcons;
+
+    [SerializeField]
+    private string[] weaponNames;
+
+    private string WEAPON_TAG = "WEAPON_TAG";
+    void OnCollisionEnter(Collision collision)
+    { 
+       
+        GameObject i;
+        if (collision.gameObject.tag == WEAPON_TAG)
+        {
+            Debug.Log(collision.gameObject.name);
+            switch(collision.gameObject.name)
+            {
+                case"M4A1":
+                    Debug.Log("picked M4A1");
+                    break;
+
+            }
+
+            i = Instantiate(inventoryIcons[0]);
+            i.transform.SetParent(inventoryPanel.transform);
+        }
+
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Battle Car/Assets/Scripts/Spawn weapon/respawn.cs
+++ Battle Car/Assets/Scripts/Spawn weapon/respawn.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class respawn : MonoBehaviour {
 
-    public int respawnTime = 60;
+    public int respawnTime = 100;
+
+
+
+    [SerializeField]
+    private GameObject[] weaponParts;
 
     void OnCollisionEnter()
     {
         this.GetComponent<BoxCollider>().enabled = false;
         this.GetComponent<MeshRenderer>().enabled = false;
+        for (int i = 0; i < weaponParts.Length; i++){
+            Debug.Log("test");
+            if(weaponParts[i].GetComponent<BoxCollider>() != null)
+                weaponParts[i].GetComponent<BoxCollider>().enabled = false;
+            if (weaponParts[i].GetComponent<MeshRenderer>() != null)
+                weaponParts[i].GetComponent<MeshRenderer>().enabled = false;
+
+        }
 
         Invoke("Respawn", respawnTime);
     }
@@ -19,16 +32,16 @@ public class respawn : MonoBehaviour {
     {
         this.GetComponent<BoxCollider>().enabled = true;
         this.GetComponent<MeshRenderer>().enabled = true;
-    }
-    // Use this for initialization
-    void Start()
-    {
+        for (int i = 0; i < weaponParts.Length; i++)
+        {
 
+            if (weaponParts[i].GetComponent<BoxCollider>() != null)
+                weaponParts[i].GetComponent<BoxCollider>().enabled = true;
+            if (weaponParts[i].GetComponent<MeshRenderer>() != null)
+                weaponParts[i].GetComponent<MeshRenderer>().enabled = true;
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
 }
